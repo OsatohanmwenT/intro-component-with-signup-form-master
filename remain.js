76d671c90e00ea415afe2710e98d7cd66305560b
OsatohanmwenT <143819001+OsatohanmwenT@users.noreply.github.com>
2024-01-11 11:33:25 +0100

    Update remain.js

--- remain.js
+++ remain.js
@@ -14,20 +14,24 @@ submitBtn.addEventListener("click", () => {
     }
     if(lastName.value == "") {
         lastName.classList.add("error-border")
+        lastName.classList.add("border")
         lastName.nextElementSibling.innerHTML = "Last Name cannot be empty"
     }
     if(emailEl.value == "") {
         emailEl.classList.add("error-border")
         emailEl.classList.add("email")
+        emailEl.classList.add("border")
         emailEl.nextElementSibling.innerHTML = "Looks like this is not an email"
         emailEl.placeholder = "email@example/com"
     }
     if(passwordEl.value === "") {
         passwordEl.classList.add("error-border")
+        passwordEl.classList.add("border")
         passwordEl.nextElementSibling.innerHTML = "Password cannot be empty"
     }
     if(passwordEl.value.length <= 7) {
         passwordEl.classList.add("error-border")
+         passwordEl.classList.add("border")
         passwordEl.nextElementSibling.innerHTML = "Must be at least 8 character"
     }
 })
